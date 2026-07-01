e92dcf7949b3b200ce98ab3b91df8c65634a5983
root <root@localhost.localdomain>
2026-07-01 00:26:41 +0000

    Change Capacitor config from TypeScript to JSON to avoid ts-node parsing failure on CI

--- capacitor.config.ts
+++
@@ -1,9 +0,0 @@
-import type { CapacitorConfig } from '@capacitor/cli';
-
-const config: CapacitorConfig = {
-  appId: 'com.prism.app',
-  appName: 'prism',
-  webDir: 'dist'
-};
-
-export default config;
